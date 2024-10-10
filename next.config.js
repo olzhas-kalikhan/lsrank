9f5f9694fea9799b3334e5b36c8a32cf849da6c6
olzhas-kalikhan <olzhas-kalikhan@outlook.com>
2024-10-09 20:43:10 -0400

    changed config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    domains: ["images.igdb.com", "imageplaceholder.net"],
+    remotePatterns: [
+      { hostname: "images.igdb.com" },
+      { hostname: "imageplaceholder.net" },
+    ],
   },
 };
 
